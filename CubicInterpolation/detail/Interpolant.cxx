92d4d847a4e2dc5bb62a78a3a59dfac2dd06b345
Maximilian Sackel <maximilian.sackel@gmx.de>
2021-01-20 11:05:36 +0100

    add position independent code for better usability

--- CubicInterpolation/detail/Interpolant.cxx
+++ CubicInterpolation/detail/Interpolant.cxx
@@ -10,7 +10,7 @@ float _find_parameter(std::function<float(float)> const &f,
   auto low = axis.GetLow();
   auto high = axis.GetHigh();
   auto func = [&f, &df](double x) { return std::make_tuple(f(x), df(x)); };
-  return boost::math::tools::newton_raphson_iterate(func, x, low, high, 10);
+  return boost::math::tools::newton_raphson_iterate(func, x, low, high, 20);
 }
 } // namespace detail
 } // namespace cubic_splines
