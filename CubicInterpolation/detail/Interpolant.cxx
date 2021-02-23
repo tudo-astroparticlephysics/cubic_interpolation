fb8367d5b318361bd2023c02aeef69c47829df44
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2021-02-23 10:38:54 +0100

    Add bisection steps before NewtonRaphson to improve initial guess (#4)
    * Add bisection steps before NewtonRaphson to improve initial guess

--- CubicInterpolation/detail/Interpolant.cxx
+++ CubicInterpolation/detail/Interpolant.cxx
@@ -10,7 +10,11 @@ double _find_parameter(std::function<double(double)> const &f,
   auto low = axis.GetLow();
   auto high = axis.GetHigh();
   auto func = [&f, &df](double x) { return std::make_tuple(f(x), df(x)); };
-  return boost::math::tools::newton_raphson_iterate(func, x, low, high, 20);
+
+  auto tol = [](double min, double max) { return (std::abs(max - min) < 1.e-2) ? true : false; };
+  std::tie(low, high) = boost::math::tools::bisect(f, low, high, tol); 
+
+  return boost::math::tools::newton_raphson_iterate(func, (low + high)/2, low, high, 20);
 }
 } // namespace detail
 } // namespace cubic_splines
