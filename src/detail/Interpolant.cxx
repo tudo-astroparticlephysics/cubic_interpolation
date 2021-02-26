afa71112b740cb27c6161cdf6d1eca227d7b573b
Maximilian Sackel <maximilian.sackel@udo.edu>
2021-02-26 11:39:20 +0100

    Improve find parameter method. In general the initial guess can't be negleted. There are cases where no change of sign exists but a solution. This solution could be dependent of the initial guess, but is often better than nothing. If no sensible guess is know, just pass a nan and the parameter area will be shrinked automatically.

--- src/detail/Interpolant.cxx
+++ src/detail/Interpolant.cxx
@@ -5,16 +5,21 @@
 namespace cubic_splines {
 namespace detail {
 double _find_parameter(std::function<double(double)> const &f,
-                      std::function<double(double)> const &df, double x,
-                      Axis const &axis) {
+                       std::function<double(double)> const &df, Axis const &axis,
+                       double initial_guess) {
   auto low = axis.GetLow();
   auto high = axis.GetHigh();
   auto func = [&f, &df](double x) { return std::make_tuple(f(x), df(x)); };
 
-  auto tol = [](double min, double max) { return (std::abs(max - min) < 1.e-2) ? true : false; };
-  std::tie(low, high) = boost::math::tools::bisect(f, low, high, tol); 
+  if (std::isnan(initial_guess)) {
+    auto tol = [](double min, double max) {
+      return (std::abs(max - min) < 1.e-2) ? true : false;
+    };
+    std::tie(low, high) = boost::math::tools::bisect(f, low, high, tol);
+    initial_guess = (low + high) / 2;
+  }
 
-  return boost::math::tools::newton_raphson_iterate(func, (low + high)/2, low, high, 20);
+  return boost::math::tools::newton_raphson_iterate(func, initial_guess, low, high, 20);
 }
 } // namespace detail
 } // namespace cubic_splines
--- tests/TestFindParameter.cpp
+++ tests/TestFindParameter.cpp
@@ -13,11 +13,11 @@ std::mt19937 gen(rd());
 TEST(find_parameter, CubicSplines) {
   constexpr static size_t N = 100;
   auto func = [](double x) { return x * x * x ; };
-  auto def = cubic_splines::CubicSplines::Definition();
+  auto def = cubic_splines::CubicSplines<double>::Definition();
   def.f = func;
-  auto xaxis = cubic_splines::LinAxis(-1, 1, N);
-  def.axis = std::make_unique<cubic_splines::LinAxis>(xaxis);
-  auto spline = cubic_splines::Interpolant<cubic_splines::CubicSplines>(std::move(def), "", "");
+  auto xaxis = cubic_splines::LinAxis<double>(-1, 1, N);
+  def.axis = std::make_unique<cubic_splines::LinAxis<double>>(xaxis);
+  auto spline = cubic_splines::Interpolant<cubic_splines::CubicSplines<double>>(std::move(def), "", "");
   std::uniform_real_distribution<double> dis(0, N-1);
   for (int i = 0; i < 10'000; ++i) {
     auto x = xaxis.back_transform(dis(gen));
@@ -31,14 +31,14 @@ TEST(find_parameter, CubicSplines) {
 TEST(find_parameter, BicubicSplines) {
   constexpr static size_t N = 100;
   auto func = [](double x1, double x2) { return x1 * x1 + x2; };
-  auto def = cubic_splines::BicubicSplines::Definition();
+  auto def = cubic_splines::BicubicSplines<double>::Definition();
   def.f = func;
-  def.f_trafo = std::make_unique<cubic_splines::ExpAxis>(1, 0);
-  auto xaxis = cubic_splines::LinAxis(-1, 1, N);
-  auto yaxis = cubic_splines::ExpAxis(1.e0, 1e2, N);
-  def.axis[0] = std::make_unique<cubic_splines::LinAxis>(xaxis);
-  def.axis[1] = std::make_unique<cubic_splines::ExpAxis>(yaxis);
-  auto spline = cubic_splines::Interpolant<cubic_splines::BicubicSplines>(std::move(def), "", "");
+  def.f_trafo = std::make_unique<cubic_splines::ExpAxis<double>>(1, 0);
+  auto xaxis = cubic_splines::LinAxis<double>(-1, 1, N);
+  auto yaxis = cubic_splines::ExpAxis<double>(1.e0, 1e2, N);
+  def.axis[0] = std::make_unique<cubic_splines::LinAxis<double>>(xaxis);
+  def.axis[1] = std::make_unique<cubic_splines::ExpAxis<double>>(yaxis);
+  auto spline = cubic_splines::Interpolant<cubic_splines::BicubicSplines<double>>(std::move(def), "", "");
   std::uniform_real_distribution<double> dis(0, N-1);
   for (int i = 0; i < 10'000; ++i) {
     auto x = xaxis.back_transform(dis(gen));
