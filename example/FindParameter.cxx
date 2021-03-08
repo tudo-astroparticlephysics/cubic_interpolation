5175d3044deb73819210bd56cf05a3c2614f94fa
Maximilian Sackel <maximilian.sackel@udo.edu>
2021-03-08 11:46:53 +0100

    update find parameter example

--- example/FindParameter.cxx
+++ example/FindParameter.cxx
@@ -1,11 +1,13 @@
 #include <cmath>
 #include <cstdlib>
+#include <array>
 #include <iostream>
 #include <memory>
 
 #include "CubicInterpolation/Axis.h"
 #include "CubicInterpolation/BicubicSplines.h"
 #include "CubicInterpolation/CubicSplines.h"
+#include "CubicInterpolation/FindParameter.hpp"
 #include "CubicInterpolation/Interpolant.h"
 
 double func1dim(double x) { return x * x + x; }
@@ -18,23 +20,25 @@ int main() {
   auto inter1d = cubic_splines::Interpolant<cubic_splines::CubicSplines<double>>(
       std::move(def1d), "", "");
   auto function_value = 1.f;
-  auto guess = 1.f;
+  auto guess = cubic_splines::ParameterGuess<double>{.x = 1.f};
 
-  auto x = find_parameter(inter1d, function_value, guess, NAN, NAN);
+  auto x = find_parameter(inter1d, function_value, guess);
   std::cout << "f(x) = " << function_value << " -> x = " << x << std::endl;
 
   auto def2d = cubic_splines::BicubicSplines<double>::Definition();
   def2d.f = func2dim;
-  def2d.axis[0] = std::make_unique<cubic_splines::LinAxis<double>>(1.e-5f, 1.e1f, (size_t)10);
-  def2d.axis[1] = std::make_unique<cubic_splines::LinAxis<double>>(1.e-5f, 1.e1f, (size_t)10);
-  auto inter2d = cubic_splines::Interpolant<cubic_splines::BicubicSplines<double>>(std::move(def2d), "", "");
+  def2d.axis[0] =
+      std::make_unique<cubic_splines::LinAxis<double>>(1.e-5f, 1.e1f, (size_t)10);
+  def2d.axis[1] =
+      std::make_unique<cubic_splines::LinAxis<double>>(1.e-5f, 1.e1f, (size_t)10);
+  auto inter2d = cubic_splines::Interpolant<cubic_splines::BicubicSplines<double>>(
+      std::move(def2d), "", "");
 
   function_value = 42.f;
-  auto x_val = 0.5f;
-  auto y_guess = 3.f;
-  auto searched_param = 1;
-  auto y = cubic_splines::find_parameter(inter2d, function_value,
-                    std::array<double, 2>{x_val, y_guess}, NAN, NAN, searched_param);
-  std::cout << "f(x="<< x_val <<",y) = " << function_value << " -> y = " << y << std::endl;
+  auto guess_2d = cubic_splines::ParameterGuess<std::array<double, 2>>{
+      .x = std::array<double, 2>{0.5f, 3.f}, .n = 1};
+  auto y = cubic_splines::find_parameter(inter2d, function_value, guess_2d);
+  std::cout << "f(x=" << guess_2d.x[0] << ",y) = " << function_value << " -> y = " << y
+            << std::endl;
   return 0;
 }
